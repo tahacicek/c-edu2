cbf4a25c6b9c22370b10f307991c02916ee2ec9b
Taha Çiçek <ttahacicek@gmail.com>
2023-04-06 04:54:50 +0300

    Add project files.

---
+++ ConsoleApp2/Ogrenci.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Ogrenci
+    {
+        private int numara;
+        private string isim;
+        private string soyisim;
+        private int vize1;
+        private int vize2;
+        private int final;
+        private string okul;
+
+        public Ogrenci(int _ogrno, string _isim, string _soyisim, string _okul, int _final, int _vize1, int _vize2)
+        {
+            numara = _ogrno;
+            isim = _isim;
+            soyisim = _soyisim;
+            this.okul = _okul;
+            this.final = _final;
+            this.vize1 = _vize1;
+            this.vize2 = _vize2;
+            this.final = _final;
+        }
+        public void OgrenciBilgileriGoster()
+        {
+            Console.WriteLine("Öğrenci Numarası: " + numara);
+            Console.WriteLine("Öğrenci İsmi: " + isim);
+            Console.WriteLine("Öğrenci Soyismi: " + soyisim);
+            Console.WriteLine("Öğrenci Vize 1: " + vize1);
+            Console.WriteLine("Öğrenci Vize 2: " + vize2);
+            Console.WriteLine("Öğrenci Final: " + final);
+            Console.WriteLine("Öğrenci Okul: " + okul);
+        }
+
+        public double OgrenciOrtalamasi()
+        {
+            double ortalama = (vize1 * 0.3) + (vize2 * 0.3) + (final * 0.4);
+            return ortalama;
+        }
+
+        public void OkulGetir()
+        {
+            Console.WriteLine("Öğrencinin Okul İsmi: " + okul);
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        /* Bir öğrencinin, numarası, isim, soyisim, vize1, vize2, final ve okul isimlerini tutan değişkenler olacak. Erişim
+        *belirliyicileri private olacak. Yapıcı metotlar ile değişkenlere değer ataması yapılacak. Öğrencinin ortalamasını
+        *hesaplayan bir metot olacak. Öğrencinin ortalaması 50’nin altında ise ekrana “Kaldı” yazdıran bir metot olacak.
+        *Öğrenci bilgilerini ekrana yazdıran bir metot olacak.
+        *OgrenciBilgileriGoster()
+        *ogrenciOrtalamasi()
+        *okulGetir()
+        *Öğrenci Bilgilerini Göster
+        *Öğrenci Ortalaması
+        *Okul Getir
+        *Çıkış Yap */
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci1 = new Ogrenci(21, "Taha", "Çiçek", "ADÜ", 70, 65, 92);
+
+            Console.WriteLine("Uygulamaya hoşgeldiniz.. Yapmak istediğiniz işlemi seçiniz.");
+            bool kontrol = true;
+
+            while (kontrol)
+            {
+                IslemSecimi();
+                int secim = Convert.ToInt32(Console.ReadLine());
+                switch (secim)
+                {
+                    case 0: Console.WriteLine("Yanlış bir seçim yaptınız. Lütfen tekrar deneyiniz."); break;
+                    case 1: ogrenci1.OgrenciBilgileriGoster(); break;
+                    case 2: Console.WriteLine("Öğrencinin ortalaması: " + ogrenci1.OgrenciOrtalamasi()); break;
+                    case 3: ogrenci1.OkulGetir(); break;
+                    case 4: Console.WriteLine("Çıkış yapılıyor..."); kontrol = false; break;
+                    default: Console.WriteLine("Yanlış bir seçim yaptınız. Lütfen tekrar deneyiniz."); break;
+                }
+            }
+        }
+
+        static void IslemSecimi()
+        {
+            Console.WriteLine("1- Öğrenci Bilgilerini Göster");
+            Console.WriteLine("2- Öğrenci Ortalaması");
+            Console.WriteLine("3- Okul Getir");
+            Console.WriteLine("4- Çıkış Yap");
+        }   
+    }
+}
